d46173b8d8013d235231c6d36f6812b951f8bc99
Samson Wu <samsonwu05@gmail.com>
2024-08-24 12:12:42 -0400

    enum statemachine

---
+++ Assets/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class StateMachine<EnumState> : MonoBehaviour where EnumState : Enum
+{
+    protected Dictionary<EnumState, BaseState<EnumState>> States = new Dictionary<EnumState, BaseState<EnumState>>();
+
+    protected BaseState<EnumState> CurrentState;
+
+    private void Start()
+    {
+        CurrentState.EnterState();
+    }
+
+    private void Update()
+    {
+        EnumState nextStateKey = CurrentState.GetNextState();
+
+        if (nextStateKey.Equals(CurrentState.StateKey))
+        {
+            CurrentState.UpdateState();
+        }
+        else
+        {
+            TransitionToNextState(nextStateKey);
+        }
+    }
+
+    private void TransitionToNextState(EnumState stateKey)
+    {
+        CurrentState.ExitState();
+        CurrentState = States[stateKey];
+        CurrentState.EnterState();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CurrentState.OnTriggerEnter(other);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        CurrentState.OnTriggerStay(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        CurrentState.OnTriggerExit(other);
+    }
+}
