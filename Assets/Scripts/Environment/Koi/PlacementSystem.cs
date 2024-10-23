4a8c0de3afb90db06a4c2d3be00fbe51baa1f970
tidalcurrent <sidneysteward@gmail.com>
2024-10-22 22:31:42 -0400

    Added cell-hover indicator & grid

--- Assets/Scripts/Environment/Koi/PlacementSystem.cs
+++ Assets/Scripts/Environment/Koi/PlacementSystem.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class PlacementSystem : MonoBehaviour
 {
     [SerializeField]
-    private GameObject mouseIndicator;
+    private GameObject mouseIndicator, cellIndicator;
     [SerializeField]
     private InputManager inputManager;
+    [SerializeField]
+    private Grid grid;
 
     private void Update()
     {
         Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
         mouseIndicator.transform.position = mousePosition;
+        cellIndicator.transform.position = grid.CellToWorld(gridPosition);
     }
 }
